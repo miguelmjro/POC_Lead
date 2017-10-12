496724578c5933aea4a2b9b6ddc3b53067b4111e
mrivera <mrivera@AVXBSMR00045.avanxo.local>
2017-10-12 12:23:30 -0500

    moved OneOffProcess

--- src/main/java/com/iheartmeadia/POC/OneOffProcess.java
+++ src/main/java/com/iheartmeadia/POC/OneOffProcess.java
@@ -1,7 +1,9 @@
+package com.iheartmeadia.POC;
+
 public class OneOffProcess
 {
     public static void main(String[] args)
     {
-        System.out.println("Prueba de ejecución.");
+        System.out.println("Prueba de ejecuciÃ³n.");
     }    
 }
