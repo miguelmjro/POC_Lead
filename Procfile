a595391595cdae06ce2c712b6b28ed9b4015d545
mrivera <mrivera@AVXBSMR00045.avanxo.local>
2017-10-13 08:41:41 -0500

    modified the structure and added the gitignore file

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/POC_Lead-0.0.1-SNAPSHOT.jar
-worker: sh target/bin/worker
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar web/target/POC_Lead-0.0.1-SNAPSHOT.jar
+worker: sh worker/target/bin/worker
---
+++ web/src/main/java/com/ihearmedia/POC/PocLeadApplication.java
@@ -0,0 +1,12 @@
+package com.ihearmedia.POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLeadApplication.class, args);
+	}
+}
